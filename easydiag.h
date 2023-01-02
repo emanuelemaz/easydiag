7e5c1d89ab606d086d00e06bb0c71c60919c86fe
Emanuele Mazzucchi <emanuelemaz@outlook.it>
2023-01-02 15:08:44 +0100

    Update easydiag.h

--- easydiag.h
+++ easydiag.h
@@ -133,7 +133,21 @@ class Context {
     * This function calculates the bending moment every dx points. It returns a pair of XY coordinates.
     */
     std::pair<std::vector<double>,std::vector<double>> getMpair();
-
+    /**
+    * @brief
+    * This function runs a gnuplot interactive window with the normal force diagram. Only tested on shell.
+    */
+    void Hdiag();
+    /**
+    * @brief
+    * This function runs a gnuplot interactive window with the shear force diagram. Only tested on shell.
+    */
+    void Vdiag();
+    /**
+    * @brief
+    * This function runs a gnuplot interactive window with the bending moment diagram. Only tested on shell.
+    */
+    void Mdiag();
 };
 
 
